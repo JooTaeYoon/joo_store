fcd8d97575172636753afe21eaa769a439e8bb22
jty <vividjty@gmail.com>
2026-01-10 22:47:46 +0900

    Feature(store): 손님이 옷 목록들 개발

--- src/main/java/com/joo/pro/controller/StoreController.java
+++ src/main/java/com/joo/pro/controller/StoreController.java
@@ -64,26 +64,11 @@ public class StoreController {
      * @param id
      * @return
      */
-    @GetMapping("/customer/{id}/get")
+    @GetMapping("/{id}/get")
     @Operation(summary = "손님 정보 가져오기", description = "손님 정보 가져오기")
-    public ResponseEntity<?> getCustomerInfo(@PathVariable("id") Long id) {
+    public ResponseEntity<SearchCustomerResponse> getCustomerInfo(@PathVariable("id") Long id) {
         return ResponseEntity.ok(customerService.getCustomerInfo(id));
     }
-
-    /**
-     * 손님 이름 및 번호(뒷자리 4자리)로 검색
-     *
-     * @param request
-     * @return
-     */
-    @GetMapping("/search")
-    public ResponseEntity<List<CustomerDtoResponse>> searchCustomers(@RequestParam("query") String query) throws IllegalAccessException {
-        log.info("request = {}", query);
-        List<CustomerDtoResponse> byOrderCustomerId = customerService.findByOrderCustomerId(query);
-        log.info("byOrderCustomerId = {}", byOrderCustomerId);
-        return ResponseEntity.ok(byOrderCustomerId);
-    }
-
 
     /**
      * 모든 손님 정보 가져오기
@@ -118,7 +103,7 @@ public class StoreController {
     @PostMapping("/{id}/save/clothes")
     @Operation(summary = "손님 옷 저장", description = "손님이 맡긴 옷 정보를 저장합니다.")
     public ResponseEntity<?> saveClothesToCustomer(@PathVariable("id") Long id, @RequestBody OrderRequest request) {
-        return ResponseEntity.ok(clothesService.saveClothes(id, request));
+        return ResponseEntity.ok(clothesService.saveClothes(id,  request));
     }
 
     /**
